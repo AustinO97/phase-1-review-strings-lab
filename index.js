3e229e6878e2a779072241de145bb8d4d3619635
Austin Oblow <austinoblow97@gmail.com>
2023-07-17 12:22:29 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = "Grace Hopper";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
